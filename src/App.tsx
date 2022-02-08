96a2aba936828bc49fabed06dc2c5f2fbc20530d
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-08 13:41:03 +0600

    Comp FullInpit added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React, {useState} from 'react';
 import './App.css';
+import {FullInput} from "./components/FullInput";
 
 function App() {
 
-  let [message, setMessage] = useState([
-    {message: 'Message1'},
-    {message: 'Message2'},
-    {message: 'Message3'}
-  ])
+    let [message, setMessage] = useState([
+        {message: 'Message1'},
+        {message: 'Message2'},
+        {message: 'Message3'}
+    ])
 
-  return (
-    <div>
+    return (
         <div>
-            <input />
-            <button>x</button>
+            <FullInput/>
+            {message.map((el, index) => <div key={index}>{el.message}</div>)}
         </div>
-
-      {message.map((el, index) => <div key={index}>{el.message}</div>)}
-
-    </div>
-  );
+    );
 }
 
 export default App;
