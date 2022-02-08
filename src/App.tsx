52f57962ee00bb9863bd1ad9ecb226627ee8eec5
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-08 15:04:49 +0600

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,14 @@ function App() {
         {message: 'Message3'}
     ])
 
+    let addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage, ...message])
+    }
+
     return (
         <div>
-            <FullInput/>
+            <FullInput addMessage={addMessage}/>
             {message.map((el, index) => <div key={index}>{el.message}</div>)}
         </div>
     );
--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,18 +1,26 @@
 import React, {ChangeEvent, useState} from 'react';
 
-export const FullInput = () => {
+
+type FullInputPropsType = {
+    addMessage: (title: string)=>void
+}
+
+export const FullInput = (props:FullInputPropsType) => {
     let [title, setTitle] = useState('')
-    console.log(title)
 
     const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
         setTitle(event.currentTarget.value)
     }
+
+    const onClickButtonHandler = () => {
+        props.addMessage(title)
+        setTitle('')
+    }
+
     return (
         <div>
-            <input onChange={onChangeInputHandler}/>
-            <button onClick={() => {
-            }}>x
-            </button>
+            <input value={title} onChange={onChangeInputHandler}/>
+            <button onClick={onClickButtonHandler}>x</button>
         </div>
     );
 };
