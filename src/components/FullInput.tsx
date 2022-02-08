0fed7e9c6c9cbe66944592ee4a916a79d3ddeed4
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-08 14:12:06 +0600

    before second task commit

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 export const FullInput = () => {
+    let [title, setTitle] = useState('')
+    console.log(title)
+
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
     return (
         <div>
-            <input />
-            <button>x</button>
+            <input onChange={onChangeInputHandler}/>
+            <button onClick={() => {
+            }}>x
+            </button>
         </div>
     );
 };
