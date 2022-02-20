37b0b55c441447c99e4f2793d981f54867f46e64
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-20 23:28:00 +0600

    microtask Filter start

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import {NewComponent} from "./components/NewComponent";
 
 function App() {
 
+
+
+
     let [message, setMessage] = useState([
         {message: 'Message1'},
         {message: 'Message2'},
@@ -50,8 +53,37 @@ function App() {
         {id: 4, name:'Benya', age:25}
     ]
 
+
+    const [money, setMoney] = useState([
+        {banknote: 'dollar', nominal: 100, number: ' a1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' z1234567890'},
+        {banknote: 'ruble', nominal: 100, number: ' w1234567890'},
+        {banknote: 'dollar', nominal: 100, number: ' e1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' c1234567890'},
+        {banknote: 'ruble', nominal: 100, number: ' r1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' x1234567890'},
+        {banknote: 'ruble', nominal: 50, number: ' v1234567890'},
+    ])
+
     return (
+
         <div className={"App"}>
+            <ul>
+                {money.map((objFromMoneyArr) => {
+                    return (
+                        <li>
+                            <span> {objFromMoneyArr.banknote}</span>
+                            <span> {objFromMoneyArr.nominal}</span>
+                            <span> {objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
+
+
+
+
             <FullInput newMessage={newMessage}/>
             {message.map((el, index) => <div key={index}>{el.message}</div>)}
 
