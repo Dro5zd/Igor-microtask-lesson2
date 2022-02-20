3624785cebd59d79f25812c7796482826e6b7f84
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-20 23:39:20 +0600

    microtask Filter start1

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,8 @@
 import React, {useState} from 'react';
 import './App.css';
-import {FullInput} from "./components/FullInput";
-import {Button} from "./components/Button";
-import {NewComponent} from "./components/NewComponent";
-
-function App() {
 
 
-
-
-    let [message, setMessage] = useState([
-        {message: 'Message1'},
-        {message: 'Message2'},
-        {message: 'Message3'}
-    ])
-
-    const newMessage = (title: string) => {
-        let addMessage = {message: title}
-        setMessage([addMessage, ...message])
-    }
-
-    const ButtonFoo1 = (subscriber: string) => {
-        console.log(subscriber)
-    }
-
-    const ButtonFoo2 = (subscriber: string) => {
-        console.log(subscriber)
-    }
-
-    const StupidButton = () => {
-        console.log('Im Stupid Button')
-    }
-
-/// Микротаска UseState
-//     let [a, setA] = useState(1)
-//
-//     const onclickPlusHandler = () => {
-//         setA(++a)
-//     }
-//     const onclickMinusHandler = () => {
-//         setA(--a)
-//     }
-//
-//     const onclick0Handler = () =>{
-//         setA(a=0)
-//     }
-
-    const students = [
-        {id: 1, name:'Senya', age:32},
-        {id: 2, name:'Venya', age:42},
-        {id: 3, name:'Jhenya', age:12},
-        {id: 4, name:'Benya', age:25}
-    ]
-
+export function App() {
 
     const [money, setMoney] = useState([
         {banknote: 'dollar', nominal: 100, number: ' a1234567890'},
@@ -66,41 +16,16 @@ function App() {
     ])
 
     return (
-
-        <div className={"App"}>
-            <ul>
-                {money.map((objFromMoneyArr) => {
-                    return (
-                        <li>
-                            <span> {objFromMoneyArr.banknote}</span>
-                            <span> {objFromMoneyArr.nominal}</span>
-                            <span> {objFromMoneyArr.number}</span>
-                        </li>
-                    )
-                })}
-            </ul>
-
-
-
-
-
-            <FullInput newMessage={newMessage}/>
-            {message.map((el, index) => <div key={index}>{el.message}</div>)}
-
-            <Button name='MyYoutubeChanel-1' callback={() => ButtonFoo1('Im Vasya')}/>
-            <Button name='MyYoutubeChanel-2' callback={() => ButtonFoo2('Im Petya')}/>
-            <Button name='Stupid Button' callback={StupidButton}/>
-
-
-            {/*<h1>{a}</h1>*/}
-            {/*<button onClick={onclickPlusHandler}> +++</button>*/}
-            {/*<button onClick={onclick0Handler}> 0 </button>*/}
-            {/*<button onClick={onclickMinusHandler}> --- </button>*/}
-
-
-            <NewComponent students={students}/>
-        </div>
-    );
+        <ul>
+            {money.map((objFromMoneyArr, index) => {
+                return (
+                    <li key={index}>
+                        <span> {objFromMoneyArr.banknote}</span>
+                        <span> {objFromMoneyArr.nominal}</span>
+                        <span> {objFromMoneyArr.number}</span>
+                    </li>
+                )
+            })}
+        </ul>
+    )
 }
-
-export default App;
